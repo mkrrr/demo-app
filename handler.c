f7c7ad90290ee131d321403b6c86bc90eccc937f
OlivierDekkers <olivierdekkers@hotmail.com>
2017-09-04 14:28:04 +0200

    Added 2 questions and answers in handler.c

--- handler.c
+++ handler.c
@@ -8,6 +8,8 @@
 
 #define LIKE_CHEESE "Do you like cheese?"
 #define LIKE_COFFEE "Do you like coffee?"
+#define LIKE_CHOCOLATE "Do you like chocolate?"
+#define FIRST_DAY "How was your first day at altran?"
 #define FIX "Will you fix this bug on the master branch now please?"
 
 struct handler{
@@ -30,6 +32,10 @@ void handler_cmd(void *priv, const char *cmd, char *response, int resp_len)
         sprintf(response, "Of course!");
     }else if(strncmp(cmd, FIX, strlen(FIX)) == 0){
         sprintf(response, "Allright then");
+    }else if(strncmp(cmd, LIKE_CHOCOLOTE, strlen(FIX)) == 0){
+        sprintf(response, "Yes, I prefer milk chocolate over pure");
+    }else if(strncmp(cmd, FIRST_DAY, strlen(FIX)) == 0){
+        sprintf(response, "It was very interesting. Especially the demo's");
     }else{
         h->cmds_rcv_err++;
         sprintf(response, "Wuuuuut?");
